4fd6b280020eacc854bc16de02a0e5a586217232
Craig Johnson <craigdev937@gmail.com>
2024-07-04 14:53:14 -0400

    Testing gh-pages

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import React from "react";
+import "./Footer.css";
+export const Footer = () => {
+    return (_jsx(React.Fragment, { children: _jsx("footer", { className: "attribution", children: _jsxs("p", { children: ["Challenge for", _jsx("a", { href: "https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm", target: "_blank", children: "Frontend Mentor" }), "created by", _jsx("a", { href: "https://www.frontendmentor.io/profile/craigdev937", target: "_blank", children: "Craig Johnson" })] }) }) }));
+};
---
+++ src/containers/Main.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import React from "react";
+export const Main = () => {
+    return (_jsxs(React.Fragment, { children: [_jsx("h1", { children: "Main" }), _jsx("p", { children: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque facere nisi saepe debitis molestias expedita corporis fuga, libero voluptate ipsum voluptas praesentium placeat quis quos architecto, sit ad nostrum numquam?" })] }));
+};
